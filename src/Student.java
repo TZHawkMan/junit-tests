7970f489ab8843950866c7d34cf1113d3f8f411e
Tianrun Zhang <zhang_tianrun@yahoo.com>
2023-01-13 14:49:12 -0600

    setting it up

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    long id;
+    String name;
+    ArrayList<integer> grades;
+
+
+    }
+}
---
+++ test/StudentTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class StudentTest {
+
+    @Test
+    public void testGetId{
+
+    }
+    @Test
+    public void testGetName{
+
+    }
+    @Test
+    public void testAddGrade{
+
+    }
+    @Test
+    public void testGetGrades{
+
+    }
+    @Test
+    public void testGetGradesAverage{
+
+    }
+}
+
