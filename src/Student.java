51584d7e7526815dac951311f5c9986e8cc84c26
Tianrun Zhang <zhang_tianrun@yahoo.com>
2023-01-18 09:05:45 -0600

    updated

--- src/Student.java
+++ src/Student.java
@@ -4,8 +4,29 @@ public class Student {
 
     long id;
     String name;
-    ArrayList<integer> grades;
+    ArrayList<Integer> grades;
 
+    public Student(long id, String name){
+        this.id = id;
+        this.name = name;
+        this. grades = new ArrayList<>();
+
+    }
+
+//   getter and setter:
+    public long getId(){
+
+    }
+    public String getName(){
+
+    }
+    public void addGrade(int grade){
+
+    }
+    public ArrayList<Integer> getGrades(){
+
+    }
+    public double getGradeAverage(){
 
     }
 }
